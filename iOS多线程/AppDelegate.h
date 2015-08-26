36e69ddea47b508c4a83bf47a6578f2d3cc293ea
goodheart <1075623679@qq.com>
2015-08-26 16:23:47 +0800

    init project

---
+++ iOS多线程/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS多线程
+//
+//  Created by 马健Jane on 15/8/26.
+//  Copyright (c) 2015年 HSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS多线程/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS多线程
+//
+//  Created by 马健Jane on 15/8/26.
+//  Copyright (c) 2015年 HSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
